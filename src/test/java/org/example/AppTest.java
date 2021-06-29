5988b178945a19097e12223a3c7073737fb53db7
olbe0615 <oleg.berest@NetCracker.com>
2021-06-29 10:49:48 +0300

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+    }
+
+    @Test
+    public void shouldReturnSum()
+    {
+        App app1 = new App();
+        assertEquals(app1.testSum(2, 2), 4);
+        assertEquals(app1.testSum(-1, 2), 1);
+        assertEquals(app1.testSum(9, 2), 11);
+    }
+}
